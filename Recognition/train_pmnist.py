66bc1e5292390878f3e74bb00c339b0adbf6c313
azhar shaikh <44742531+rahzaazhar@users.noreply.github.com>
2020-04-28 16:52:11 +0530

    Add files via upload

--- Recognition/train_pmnist.py
+++ Recognition/train_pmnist.py
@@ -1,2 +1,18 @@
 import torch
 from modelsv1 import GradCL
+
+#1)get data loaders for each task
+#2)
+
+#issues
+#1) How to compare with exsiting models what is the procedure
+#2) gradient collection with trained model or randomly initialise all models
+#	and do comparisom
+#3) method train_single(trainloader,valloader,taskname)
+
+# To compute similarity with previous models 
+# need a template network, build the templa
+# need two dataloaders one for the new task data and one for old task data corresponding to that model
+# step 1: find similar model
+# how? How do you collect gradients for this model?  
+#save template and parameters 
